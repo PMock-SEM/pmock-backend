1cba97305cd491a95ddb11985c10b8a44b0eaa85
Qige Chen <cqg186495@outlook.com>
2019-10-20 13:05:40 -0700

    [Sprint1] Add user model (#3)
    * add user model
    
    * remove updating timestamp code

--- models/User.js
+++ models/User.js
@@ -1,5 +1,58 @@
 import mongoose, { Schema } from 'mongoose';
+const BCrypt = require('bcryptjs');
+const SALT_FACTOR = 10;
 
-let userSchema = new Schema({
-  email: String
-})
+const UserSchema = new Schema({
+  firstName: String,
+  lastName: String,
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: String,
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  },
+  avatarLink: String,
+  linkedAcessToken: String
+}, {
+  timestamps: {
+    createdAt: 'createdTime',
+    updatedAt: 'updatedTime'
+  }
+});
+
+UserSchema.pre('save', function (next) {
+  let user = this;
+  let salt = BCrypt.genSaltSync(SALT_FACTOR);
+
+  if (!user.isModified('password')) {
+    return next();
+  }
+
+  BCrypt.hash(user.password, salt, (err, hash) => {
+    if (err) return next(err);
+    user.password = hash;
+    next();
+  });
+});
+
+UserSchema.methods = {
+  comparePassword: function (password, cb) {
+    bcrypt.compare(password, this.password, function (err, isMatch) {
+      if (err) return cb(err);
+      cb(null, isMatch);
+    });
+  },
+  comparePasswordSync: function (password) {
+    return BCrypt.compareSync(password, this.password);
+  }
+}
+
+export default mongoose.model('User', UserSchema);
