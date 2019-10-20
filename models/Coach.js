c1264b44f6c03916393c069e3caf0b698956f9a5
Vamsi <sai.medishetty@sv.cmu.edu>
2019-10-20 12:46:39 -0700

    Adding Coach Model

---
+++ models/Coach.js
@@ -0,0 +1,34 @@
+import mongoose, { Schema } from 'mongoose';
+
+let coachSchema = new Schema({
+  coachId: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  coachEmail: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (val) => {
+        let pattern = /^\S+@\S+$/
+        return pattern.test(val)
+      },
+      message: '{VALUE} is not a valid email'
+    }
+  },
+  password: String,
+  coachFirstName: String,
+  coachLastName: String,
+  avatarLink: String,
+  linkedAccessToken: String,
+  rating: Number,
+}, { 
+	timestamps: { 
+		createdAt: 'createdTime' 
+	}, 
+    { 
+    	updatedAt: 'updatedTime' 
+    }, 
+})
