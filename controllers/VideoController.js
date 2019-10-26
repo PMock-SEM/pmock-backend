e5a04512efbbe1f1ddc77a27d60505214c467255
akritiaggarwal <akritia@andrew.cmu.edu>
2019-10-25 17:29:41 -0700

    Adding video api code

--- controllers/VideoController.js
+++ controllers/VideoController.js
@@ -1,67 +1,16 @@
 const mongoose = require('mongoose');
 const UserService = require('../services/UserService');
-const User = require('../models/Video');
+const Video = require('../models/Video');
+const videoDb = mongoose.model('Video');
 
 class VideoController {
-    static async signIn(req, res, next) {
-        try {
-            const userParams = {
-                email: req.body.email,
-                password: req.body.password
-            };
 
-            const token = await UserService.authenticateUser(userParams);
-            if (!token) {
-                return res.status(401).json({
-                    message: 'Email and password does not match'
-                });
-            }
-            return res.status(200).json({
-                token: token,
-                message: 'Success signing in user'
-            });
-        } catch (e) {
-            console.log(e);
-        }
-    }
-
-    static async signUp(req, res, next) {
+    static async getVideosById(req, res) {
+        let userId = req.body.id;
         try {
-            const userParams = {
-                firstName: req.body.firstName,
-                lastName: req.body.lastName,
-                email: req.body.email,
-                password: req.body.password,
-                avatarLink: req.body.avatarLink
-            }
-            let newUser = await UserService.addUser(userParams);
+            let videoData = await videoDb.find({"userId":userId});
             return res.status(200).json({
-                data: newUser,
-                message: 'Success creating user'
-            });
-        } catch (e) {
-            console.log(e);
-        }
-    }
-
-    static async getUsers(req, res, next) {
-        try {
-            let users = await UserService.getUsers();
-            return res.status(200).json({
-                data: users,
-                message: 'Success getting users'
-            });
-        } catch (e) {
-            console.log(e);
-        }
-    }
-
-    static async getUserById(req, res) {
-        const id = req.params.id;
-        try {
-            let user = await UserService.getUserById(id);
-            return res.status(200).json({
-                data: user,
+                data: videoData,
                 message: 'Success getting users by id'
             });
         } catch (e) {
@@ -69,17 +18,12 @@ class VideoController {
         }
     }
 
-    static async updateUserById(req, res) {
+    static async uploadVideo(req, res) {
         try {
-            const userId = req.params.id;
-            const userParams = {
-                firstName: req.body.firstName,
-                lastName: req.body.lastName,
-                email: req.body.email,
-                password: req.body.password,
-                avatarLink: req.body.avatarLink
-            };
-            const user = await UserService.updateUserById(userId, userParams);
+            const videoUri = req.body.videoURI;
+            const userId = req.body.userId;
+
+            
             return res.status(200).json({
                 data: user,
                 message: 'Success updating user by id'
@@ -88,19 +32,6 @@ class VideoController {
             console.log(e);
         }
     }
-
-    static async deleteUserById(req, res) {
-        const id = req.params.id;
-        try {
-            await UserService.removeUserById(id);
-            return res.status(200).json({
-                message: 'Success deleting user by id'
-            });
-        } catch (e) {
-            console.log(e);
-        }
-    }
-
 
 }
 
--- routes/videos.js
+++ routes/videos.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', videoController.getVideos);
+router.get('/getVideos', videoController.getVideosById);
 
 router.post('/upload', videoController.uploadVideo);
 
